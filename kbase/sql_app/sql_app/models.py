f6e97b7c036e083aabca5c4657fbf210266788c1
Alexander Junge <alexander.junge@gmail.com>
2020-02-04 08:54:12 +0100

    Add User and UserRating SqlAlchemy models

--- kbase/sql_app/sql_app/models.py
+++ kbase/sql_app/sql_app/models.py
@@ -1,7 +1,8 @@
-from datetime import date
+from datetime import date, datetime
 from typing import List
 
 import sqlalchemy as sa
+from sqlalchemy.orm import relationship
 
 from sqlalchemy.dialects.postgresql import ARRAY
 
@@ -11,7 +12,7 @@ from .database import SqlAlchemyBase
 class Article(SqlAlchemyBase):
     __tablename__ = "articles"
 
-    id: str = sa.Column(sa.String, primary_key=True)
+    id: str = sa.Column(sa.String, primary_key=True, index=True)
     version: str = sa.Column(sa.String)
     source: str = sa.Column(sa.String)
     journal: str = sa.Column(sa.String)
@@ -27,3 +28,32 @@ class Article(SqlAlchemyBase):
     language: str = sa.Column(sa.String)
     keywords: List[str] = sa.Column(ARRAY(sa.String, dimensions=1))
     references: List[str] = sa.Column(ARRAY(sa.String, dimensions=1))
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = "users"
+
+    id = sa.Column(sa.Integer, primary_key=True, index=True)
+    email = sa.Column(sa.String, unique=True, index=True)
+    name = sa.Column(sa.String)
+    hashed_password = sa.Column(sa.String)
+    created_date = sa.Column(sa.DateTime, default=datetime.now, index=True)
+    last_login = sa.Column(sa.DateTime, default=datetime.now, index=True)
+    is_active = sa.Column(sa.Boolean, default=True)
+
+    ratings = relationship("UserRating", back_populates="rated_by")
+
+
+class UserRating(SqlAlchemyBase):
+    __tablename__ = "user_ratings"
+
+    id = sa.Column(sa.Integer, primary_key=True, index=True)
+    article_id = sa.Column(
+        sa.String, sa.ForeignKey("articles.id"), nullable=False, index=True
+    )
+    value = sa.Column(sa.Float, nullable=False)
+    user_id = sa.Column(
+        sa.Integer, sa.ForeignKey("users.id"), nullable=False, index=True
+    )
+
+    rated_by = relationship("User", back_populates="ratings")
