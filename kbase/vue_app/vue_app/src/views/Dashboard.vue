f0ba668139e13ecb0f43876488bff1e67b4cf491
Alexander Junge <alexander.junge@gmail.com>
2020-02-16 20:45:22 +0100

    Changing directions and trying to parse ratings directly

--- kbase/vue_app/vue_app/src/views/Dashboard.vue
+++ kbase/vue_app/vue_app/src/views/Dashboard.vue
@@ -90,7 +90,7 @@
                 depressed
                 small
                 dark
-                :outlined="user_liked_article(article.id, 1)"
+                :outlined="user_liked_article(article.id, 1, article.ratings)"
                 color="light-blue darken-3"
                 v-on:click="rate(article.id, 1, 1.0)"
               >
@@ -105,7 +105,7 @@
                 depressed
                 small
                 dark
-                :outlined="user_disliked_article(article.id, 1)"
+                :outlined="user_disliked_article(article.id, 1, article.ratings)"
                 color="light-blue darken-3"
                 v-on:click="rate(article.id, 1, -1.0)"
               >
@@ -162,39 +162,17 @@ export default {
           console.log(error);
         });
     },
-    user_rated_article: function(article, user) {
-      axios
-        .get("http://localhost:8087/users/" + user + "/user_ratings/" + article)
-        .catch(function(error) {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        })
-        .then(function(response) {
-          //console.log(response.data.value);
-          return response.data.value;
-        });
+    get_user_article_rating: function(article, user, ratings) {
+      console.log("Article: " + article);
+      console.log("User: " + user);
+      console.log("Ratings: " + ratings);
+      return 1; // TODO pull this from ratings
     },
-    user_liked_article: function(article, user) {
-      const res = this.user_rated_article(article, user);
-      console.log(res);
-      return this.user_rated_article(article, user) == 1;
+    user_liked_article: function(article, user, ratings) {
+      return this.get_user_article_rating(article, user, ratings) == 1;
     },
-    user_disliked_article: function(article, user) {
-      return this.user_rated_article(article, user, -1);
+    user_disliked_article: function(article, user, ratings) {
+      return this.get_user_article_rating(article, user, ratings) == -1;
     }
   }
 };
