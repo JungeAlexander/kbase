28a07bd56226d610f7224e70fe41005bef55255b
Alexander Junge <alexander.junge@gmail.com>
2020-03-25 19:48:11 +0100

    First attempt implementing recommend() method

--- kbase/sql_app/sql_app/tools.py
+++ kbase/sql_app/sql_app/tools.py
@@ -1,12 +1,12 @@
 from typing import Callable, List
 
-from . import crud, main, models
+from . import main, models
 
 
 def get_article_texts(article_ids: List[str]) -> List[str]:
     texts = []
     for aid in article_ids:
-        article: models.Article = main.read_article(aid)
+        article = main.read_article(aid)
         title = article.title
         if title[-1] not in "!?.":
             title += "."
@@ -19,8 +19,26 @@ def get_article_texts(article_ids: List[str]) -> List[str]:
 def get_articles_by_rating(
     user: models.User, score_fun: Callable[[float], bool]
 ) -> List[str]:
-    pass
+    article_ids = []
+    for r in user.ratings:
+        if score_fun(r.value):
+            article_ids.append(r.article_id)
+    return article_ids
 
 
-def get_articles_not_rated(user: models.User) -> List[str]:
-    pass
+def get_articles_not_rated(
+    user: models.User, articles_returned: int = 100, skip: int = 0, limit: int = 1000
+) -> List[str]:
+    article_ids = []
+    user_id = user.id
+    for a in main.read_articles(skip=skip, limit=limit):
+        if len(article_ids) >= articles_returned:
+            return article_ids
+        rated_by_user = False
+        for r in a.ratings:
+            if user_id == r.user_id:
+                rated_by_user = True
+                break
+        if not rated_by_user:
+            article_ids.append(a.id)
+    return article_ids
