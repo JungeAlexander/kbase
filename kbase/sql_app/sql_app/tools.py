fe95cabc389f54b218d3e9b207a2fb5818874ec3
Alexander Junge <alexander.junge@gmail.com>
2020-03-28 08:35:25 +0100

    Use session scope for recommender

--- kbase/sql_app/sql_app/tools.py
+++ kbase/sql_app/sql_app/tools.py
@@ -1,18 +1,19 @@
 from typing import Callable, List
 
-from . import main, models
+from . import database, main, models
 
 
 def get_article_texts(article_ids: List[str]) -> List[str]:
     texts = []
-    for aid in article_ids:
-        article = main.read_article(aid)
-        title = article.title.strip()
-        if title[-1] not in "!?.":
-            title += "."
-        summary = article.summary.strip()
-        text = title + " " + summary
-        texts.append(text)
+    with database.session_scope() as session:
+        for aid in article_ids:
+            article = main.read_article(aid, db=session)
+            title = article.title.strip()
+            if title[-1] not in "!?.":
+                title += "."
+            summary = article.summary.strip()
+            text = title + " " + summary
+            texts.append(text)
     return texts
 
 
@@ -31,14 +32,15 @@ def get_articles_not_rated(
 ) -> List[str]:
     article_ids = []
     user_id = user.id
-    for a in main.read_articles(skip=skip, limit=limit):
-        if len(article_ids) >= articles_returned:
-            return article_ids
-        rated_by_user = False
-        for r in a.ratings:
-            if user_id == r.user_id:
-                rated_by_user = True
-                break
-        if not rated_by_user:
-            article_ids.append(a.id)
+    with database.session_scope() as session:
+        for a in main.read_articles(skip=skip, limit=limit, db=session):
+            if len(article_ids) >= articles_returned:
+                return article_ids
+            rated_by_user = False
+            for r in a.ratings:
+                if user_id == r.user_id:
+                    rated_by_user = True
+                    break
+            if not rated_by_user:
+                article_ids.append(a.id)
     return article_ids
